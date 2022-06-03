fd0d068af6cd3720a2d90db7b9c268fbafa83afd
Tushar Biswas <tusarbiswas888@gmail.com>
2022-06-04 03:55:52 +0600

    end-bannder section design complete

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import '../styles/Banner.css';
+import { Carousel } from 'react-bootstrap';
+
+const Banner = ({ banner }) => {
+    return (
+        <Carousel fade>
+            {banner.map((item, index) => (
+                <Carousel.Item key={index} interval={1000} keyboard={true} id="banner" >
+                    <img src={item.image} alt={'banner ' + index} className="d-block w-100" id="bannerImage" />
+                    <Carousel.Caption>
+                        <h3>{item.name}</h3>
+                        <p>{item.description}</p>
+                        <p>{item.source}</p>
+                        <u>read more</u>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
+}
+
+export default Banner;
